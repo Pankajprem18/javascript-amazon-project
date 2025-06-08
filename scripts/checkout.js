d81dd65601c625d448bbf8f89653d39f86bb31db
Pankaj Kumar Mandal <pankajkrp16@.com>
2025-06-09 00:05:52 +0530

    Delete products from HTMl when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML +=
-        `<div class="cart-item-container">
+        `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -97,10 +97,15 @@ cart.forEach((cartItem) => {
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
 
-    document.querySelectorAll('.js-delete-link')
-    .forEach((link)=>{
-        link.addEventListener('click', ()=>{
-           const productId = link.dataset.productId;
-           removeFromCart(productId);
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+
+          const container =  document.querySelector(
+                `.js-cart-item-container-${productId}`
+            );
+            container.remove();
         });
     });
